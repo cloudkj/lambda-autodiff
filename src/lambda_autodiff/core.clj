e9efa51bbfe1d89bc4b6996d1d40384672c50fd7
Kelvin Jiang <cloudkj@users.noreply.github.com>
2024-06-29 14:44:54 -0700

    Initial version of reverse autodiff with two operators

--- src/lambda_autodiff/core.clj
+++ src/lambda_autodiff/core.clj
@@ -1,6 +1,55 @@
 (ns lambda-autodiff.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn make-node
+  "Creates a node in the computational graph"
+  ([value]
+   (make-node value nil [] []))
+  ([value label children edges]
+   {:value value
+    :label label
+    :children children
+    :edges edges}))
+
+(defn add
+  [a b]
+  (make-node (+ (:value a) (:value b))
+             "+"
+             [a b]
+             [1 1]))
+
+(defn mul
+  [a b]
+  (make-node (* (:value a) (:value b))
+             "*"
+             [a b]
+             [(:value b) (:value a)]))
+
+(defn differentiate
+  "Returns a map of nodes to partial derivative values"
+  [root]
+  (loop [gradients {}
+         stack (list [root 1])]
+    (if (empty? stack)
+      gradients
+      (let [[node product] (peek stack)
+            [gradients' stack'] (reduce (fn [[gs st] [child weight]]
+                                          ;;(println (:label node) "---" weight "--->" (:label child))
+                                          [(assoc gs child (+ (get gs child 0) (* product weight)))
+                                           (conj st [child (* product weight)])])
+                                        [gradients (pop stack)]
+                                        (map vector (:children node) (:edges node)))]
+        (recur gradients' stack')))))
+
+(defn -main
+  [& args]
+  (let [a (make-node 4 "a" [] [])
+        b (make-node 3 "b" [] [])
+        e (make-node 2 "e" [] [])
+        c (add a b)
+        d (mul a c)
+        f (mul b e)
+        g (add d f)
+        grads (differentiate g)]
+    (println (:value g))
+    (doseq [[node grad] grads]
+      (println (:label node) "\t" grad))))
